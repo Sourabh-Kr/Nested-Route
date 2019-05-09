bbb82d1f226fedc261bda4f5a1093486e2f306b1
sourabh kumar <sourabh.kr001@gmail.com>
2019-05-10 02:26:06 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import{Route, Link, BrowserRouter as Router} from 'react-router-dom'
+const Home = ()=>(
+  <div>
+     <h2>Home</h2>
+  </div>
+)
+const About = ()=>(
+  <div>
+     <h2>About</h2>
+  </div>
+)
+const Topic= ({match})=>console.log('match', match) || (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+const Topics = ({match})=>console.log('match',match)||(
+  <div>
+     <h2>Topics</h2>
+     <ul>
+       <li><Link to={`${match.url}/rendering`}>
+        Rendering with react
+       </Link></li>
+       <li><Link to={`${match.url}/component`}>
+        Components
+       </Link></li>
+       <li><Link to={`${match.url}/prop-v-state`}>
+        Props vs state
+       </Link></li>
+     </ul>
+     <Route path="/Topics/:topicId" component={Topic}></Route>
+     {/* <Route path="/Topics/components" component={Topic}></Route> */}
+     {/* <Route path="/Topics/props-v-state" component={Topic}></Route> */}
+     <Route exact path={match.url} render={()=>(
+       <h3>please select the topic</h3>
+     )}></Route>
+  </div>
+)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/About">About</Link></li>
+          <li><Link to="/Topics">Topics</Link></li>
+        </ul>
+        <hr></hr>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/About" component={About}></Route>
+        <Route path="/Topics" component={Topics}></Route>
+      </div>
+
+    </Router>
+    
   );
 }
 
